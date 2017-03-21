0ec5c8bcc63b96d03a9f30cd34bb5a0975183439
Joao Carreira <jfmcarreira@gmail.com>
2017-03-21 01:31:38 +0000

    Change bash-it theme to powerline-carreira

--- bash-it/.bashrc.d/bash-it.bash
+++ bash-it/.bashrc.d/bash-it.bash
@@ -11,7 +11,8 @@ USE_BASH_IT=true
 export BASH_IT=~/.bashrc.d/bash-it
 
 # location ${BASH_IT}/themes/
-export BASH_IT_THEME='carreira'
+#export BASH_IT_THEME='carreira'
+export BASH_IT_THEME='powerline-carreira'
 
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
