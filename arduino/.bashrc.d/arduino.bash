d6907574bec562ef10df363bdd406b1159044212
Joao Carreira <jfmcarreira@gmail.com>
2017-11-15 02:36:12 +0000

    Add arduino conf files

---
+++ arduino/.bashrc.d/arduino.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+##################################################
+# Arduino stuff
+##################################################
+
+export ARDUINO_DIR=/opt/arduino
+export ARDMK_DIR=/opt/arduino-mk
+export AVR_TOOLS_DIR=/opt/arduino/hardware/tools/avr/
