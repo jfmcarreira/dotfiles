c900cba939dac2d6a14b75e59cc332df76c3bdda
Joao Carreira <jfmcarreira@gmail.com>
2021-02-14 19:24:15 +0000

    Add custom zsh completion

--- zsh/.zshrc.d/13-compinstall.zsh
+++ zsh/.zshrc.d/13-compinstall.zsh
@@ -1,8 +1,14 @@
 # The following lines were added by compinstall
-zstyle :compinstall filename '/home/joaoc/.zshrc'
+zstyle :compinstall filename '$HOME/.zshrc'
 
 autoload -Uz compinit
 compinit
 
 autoload bashcompinit
 bashcompinit
+
+if [ -d ~/.zsh_completion.d ] ; then
+    for sh in ~/.zsh_completion.d/*; do
+        [[ -r ${sh} ]] && source "${sh}"
+    done
+fi
