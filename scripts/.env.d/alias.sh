3158ad7f7e72420a3343b7a3e87d5d0bee408ce8
Joao Carreira <jfmcarreira@gmail.com>
2021-02-21 00:59:55 +0000

    Fix alias and fix ZSH bug

--- scripts/.env.d/alias.sh
+++ scripts/.env.d/alias.sh
@@ -21,4 +21,4 @@ alias find-cursor='find-cursor --repeat 0 --follow --distance 1 --line-width 16 
 
 # Git
 alias gs='git status'
-alias gl='git log --all --graph --decorate'
+alias gl='git log --graph --decorate'
