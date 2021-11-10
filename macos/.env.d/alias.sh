5d22928cf424d6ad3721db6ed9906d1b9675a4f4
Joao Carreira <jfmcarreira@gmail.com>
2021-11-10 16:35:43 +0000

    Add macos specific config

---
+++ macos/.env.d/alias.sh
@@ -0,0 +1,9 @@
+##################################################
+# Alias
+##################################################
+alias ls='ls -h -G'
+alias ll='ls -hvl -G'
+
+# Git
+alias gs='git status'
+alias gl='git log --graph --decorate'
