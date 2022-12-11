f28f8b1c4744344d68325f19f2582764a4ca7392
Joao Carreira <joao.carreira@soundparticles.com>
2022-12-11 21:59:33 +0000

    fix: updata zsh and vim

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Personal collection of dotfiles
 
+To use them you will need the following:
+- make
+- stow
+
 This collection includes my configurations of the following software (among others):
 - bash
 - git
---
+++ macos/.env.d/01-home-brew-path.sh
@@ -0,0 +1,3 @@
+PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
+#PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix llvm)/bin:$PATH"
+
