4f0c0adc57fece377b9b15a46abae21d6ddfff93
Joao Carreira <joao.carreira@soundparticles.com>
2023-04-23 17:52:30 +0100

    fix(macos): remove homebrew path

--- macos/.env.d/home-brew-path.sh
+++
@@ -1,3 +0,0 @@
-PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
-#PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix llvm)/bin:$PATH"
-
