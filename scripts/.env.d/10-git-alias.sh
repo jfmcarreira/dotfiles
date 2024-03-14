6979f71e8ace20faa90d5d5d4274fbf98f4c0210
Joao Carreira <jfmcarreira@gmail.com>
2024-03-14 21:18:34 +0000

    feat(git): update git colors and alias

--- scripts/.env.d/10-git-alias.sh
+++ scripts/.env.d/10-git-alias.sh
@@ -2,5 +2,7 @@
 alias czc='cz commit'
 alias gsl='git stash list'
 alias gp='git pull'
+alias gca='git commit --amend --no-edit'
 alias gs='git status'
 alias gl='git log --graph --decorate'
+alias gco='git checkout'
